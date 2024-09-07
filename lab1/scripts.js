4ca26bc08d555385a1019a97ed66a4dff8a86a92
Anna Glushko <150285428+SolanumMuricatum@users.noreply.github.com>
2024-09-07 13:34:25 +0300

    Update scripts.js

--- lab1/scripts.js
+++ lab1/scripts.js
@@ -10,17 +10,27 @@ const personalMovieDB = {
 };
 
 for (let i = 0; i < 2; i++) {
+  
   let lastMovie, movieRating;
 
   do {
-      lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+    lastMovie = prompt('Один из последних просмотренных фильмов?', '');
   } while (lastMovie.trim() === '' || lastMovie == null || lastMovie.length > 50);
 
+  // Проверяем, существует ли фильм
+  let movieKey = lastMovie;
+  let count = 1;
+  
+  while (personalMovieDB.movies[movieKey] !== undefined) {
+    movieKey = lastMovie + ' (' + count + ')'; // Добавляем суффикс
+    count++;
+  }
+
   do {
-      movieRating = prompt('На сколько оцените его?', '');
+    movieRating = prompt('На сколько оцените его?', '');
   } while (movieRating.trim() === '' || movieRating == null || isNaN(movieRating));
 
-  personalMovieDB.movies[lastMovie] = movieRating; 
+  personalMovieDB.movies[movieKey] = movieRating; // Сохраняем с уникальным ключом
 }
 
 console.log(personalMovieDB)
