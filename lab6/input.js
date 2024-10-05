3dfaebc3a02e2f6b2e3967bf6688131abc113092
Pepino <hanna.hlushko.2005@gmail.com>
2024-10-05 23:39:08 +0300

    help me

--- lab6/input.js
+++ lab6/input.js
@@ -47,10 +47,11 @@ document.addEventListener('touchmove', (e) => {
             document.addEventListener('click', funClick);
         }
     } else if (e.touches.length === 2){
-        document.removeEventListener("touchmove", moveAt);
+       
         element.style.left = initialPosition.left;
         element.style.top = initialPosition.top;
-        isFollowing = lastFollowing;
+        //isFollowing = lastFollowing;
+        document.removeEventListener("touchmove", moveAt);
         document.removeEventListener('touchend', (e));
     }
 });
